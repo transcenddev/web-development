9bfe29c5bbfe146a82f144f7024044cb93d9f6c9
r3kashi <r3kashit@gmail.com>
2022-09-23 09:28:51 +0800

    add initial files

---
+++ javascript-full-course-for-beginners/main.js
@@ -0,0 +1,5 @@
+alert('Hello World!');   // display an alert box with the message 'Hello World!
+
+console.log('Hello World!'); // display a string of 'Hello World!' at the top of    the console
+console.error('This is an error!'); // display an error message at the bottom of the console
+console.warn('This is an warning!'); // display an warning message at the bottom
